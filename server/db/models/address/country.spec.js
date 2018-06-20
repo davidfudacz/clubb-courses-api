d0279a6331ec413a0efe2ce8569f580048e0f678
David Fudacz <davidfudacz@gmail.com>
2018-06-20 14:37:36 -0500

    added tests for city and country

---
+++ server/db/models/address/country.spec.js
@@ -0,0 +1,94 @@
+/* global describe beforeEach it */
+
+const { expect } = require('chai')
+const db = require('../../index')
+const { Country } = require('../../models')
+
+describe('Country model', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  describe('validations', () => {
+    describe('do not allow null', () => {
+      it('for name', async () => {
+        let error
+        try {
+          await Country.create({
+            abbreviation: 'USA',
+          })
+        }
+        catch (err) {
+          error = err
+        }
+        expect(error.name).to.be.equal('SequelizeValidationError')
+      })
+
+      it('for abbreviation', async () => {
+        let error
+        try {
+          await Country.create({
+            name: 'Unites States of America',
+          })
+        }
+        catch (err) {
+          error = err
+        }
+        expect(error.name).to.be.equal('SequelizeValidationError')
+      })
+    }) // end describe do not allow null
+
+    describe('do not allow empty', () => {
+      it('for name', async () => {
+        let error
+        try {
+          await Country.create({
+            name: '',
+            abbreviation: 'USA',
+          })
+        }
+        catch (err) {
+          error = err
+        }
+        expect(error.name).to.be.equal('SequelizeValidationError')
+      })
+
+      it('for abbreviation', async () => {
+        let error
+        try {
+          await Country.create({
+            name: 'United States of America',
+            abbreviation: '',
+          })
+        }
+        catch (err) {
+          error = err
+        }
+        expect(error.name).to.be.equal('SequelizeValidationError')
+      })
+    }) // end describe do not allow empty
+  }) // end describe validations
+
+  describe('creation', () => {
+    describe('creating an instance', () => {
+      it('persists all columns', async () => {
+        let country, error
+        try {
+          country = await Country.create({
+            name: 'country',
+            abbreviation: 'USA'
+          })
+        }
+        catch (err) {
+          error = err
+        }
+        expect(error).to.be.an('undefined')
+        // just in case we add something and forget to test it...
+        // add in the created and updatedAt fields
+        expect(Object.keys(country.dataValues).length).to.be.equal(5)
+        expect(country.name).to.be.equal('country')
+        expect(country.abbreviation).to.be.equal('USA')
+      })
+    }) // end describe creating an instance
+  }) // end describe creations
+}) // end describe Country model
