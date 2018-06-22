9974efcb0f6cd43ce7eae9e518825770e5a28ec9
David Fudacz <davidfudacz@gmail.com>
2018-06-21 22:42:13 -0500

    webpack, babel, and all the good things

--- client/store/index.js
+++ client/store/index.js
@@ -1,9 +1,11 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import createLogger from 'redux-logger'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import User from './user'
 
 const reducer = combineReducers({
+  User,
 })
 const middleware = composeWithDevTools(applyMiddleware(
   thunkMiddleware,
