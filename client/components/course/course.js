4dd2fb961e3c35b021507d7445cce78281c2a638
David Fudacz <davidfudacz@gmail.com>
2018-06-23 23:53:42 -0500

    added build components to show architects

--- client/components/course/course.js
+++ client/components/course/course.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import YardageInfoWrapper from './yardage-info-wrapper'
+import BuildWrapper from './build-wrapper'
 
 const Course = (props) => {
   return (
-    <YardageInfoWrapper yardageInfos={props.course.yardageInfos} />
+    <div>
+      <BuildWrapper builds={props.course.builds} />
+      <YardageInfoWrapper yardageInfos={props.course.yardageInfos} />
+    </div>
   )
 }
 
